f3a84312f920cc2f744f090cddf2f99472cfa495
Brian Surowiec <brian@13degrees.com>
2020-02-10 09:14:11 -0500

    Add missing type annotation

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from "@actions/core";
 import { CommandHandler } from "./commandHandler";
 import { PermissionLevel } from "./interfaces";
 
-export async function run() {
+export async function run(): Promise<void> {
   try {
     await new CommandHandler(
       core.getInput("repo-token", { required: true }),
