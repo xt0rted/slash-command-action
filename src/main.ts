17e1cde3946deadd1508cf1e0ce5c8cb3a2b47ad
Brian Surowiec <brian@13degrees.com>
2021-11-07 16:09:53 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import * as core from "@actions/core";
+import {
+  getInput,
+  setFailed,
+} from "@actions/core";
 
 import { CommandHandler } from "./commandHandler";
 import { PermissionLevel } from "./interfaces";
@@ -6,15 +9,19 @@ import { PermissionLevel } from "./interfaces";
 export async function run(): Promise<void> {
   try {
     await new CommandHandler(
-      core.getInput("repo-token", { required: true }),
-      core.getInput("command", { required: true }),
-      core.getInput("reaction") === "true",
-      core.getInput("reaction-type"),
-      core.getInput("allow-edits") === "true",
-      core.getInput("permission-level") as PermissionLevel,
+      getInput("repo-token", { required: true }),
+      getInput("command", { required: true }),
+      getInput("reaction") === "true",
+      getInput("reaction-type"),
+      getInput("allow-edits") === "true",
+      getInput("permission-level") as PermissionLevel,
     ).process();
   } catch (error) {
-    core.setFailed(error.message);
+    if (error instanceof Error) {
+      setFailed(error.message);
+    } else {
+      throw error;
+    }
   }
 }
 
